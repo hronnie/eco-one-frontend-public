b30c1fad3a5648551da5612a52b8471811bb27c7
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2023-05-22 10:11:04 +0200

    Fixes icons and menu

--- src/app/containers/default-layout/_nav.ts
+++ src/app/containers/default-layout/_nav.ts
@@ -2,12 +2,31 @@ import {INavData} from '@coreui/angular-pro';
 
 export const navItems: INavData[] = [
     {
-        name: $localize`Dashboard`,
+        name: $localize`Kezdőlap`,
         url: '/dashboard',
-        iconComponent: {name: 'cil-speedometer'},
-        badge: {
-            color: 'info',
-            text: $localize`NEW`
-        }
-    }
+        iconComponent: {name: 'cil-speedometer'}
+    },
+    {
+        title: true,
+        name: 'Tanítványok'
+    },
+    {
+        name: 'Tanítványok',
+        url: '/smart-table',
+        iconComponent: { name: 'cil-address-book'},
+    },
+    {
+        title: true,
+        name: 'Tanfolyamok'
+    },
+    {
+        name: 'Email sablonok',
+        url: '/smart-table',
+        iconComponent: { name: 'cil-envelope-closed'},
+    },
+    {
+        name: 'Meghívók küldése',
+        url: '/smart-table',
+        iconComponent: { name: 'cil-send'},
+    },
 ];
