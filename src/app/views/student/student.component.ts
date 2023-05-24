1c3f3e87398b89dc6f55ca561348b088986018a3
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2023-05-24 19:07:20 +0200

    Flattens subscription

--- src/app/views/student/student.component.ts
+++ src/app/views/student/student.component.ts
@@ -3,6 +3,7 @@ import {MemberService} from "../../services/member.service";
 import {CenterService} from "../../services/center.service";
 import {LOCAL_STORAGE_KEY_USERNAME} from "../../constants/localStorageKeys.constant";
 import {Member} from "../../interfaces/member.model";
+import {concatMap, mergeMap, switchMap} from "rxjs";
 
 @Component({
     templateUrl: 'student.component.html',
@@ -19,17 +20,12 @@ export class StudentComponent implements OnInit{
     ngOnInit(): void {
         const currentUser: string | null = localStorage.getItem(LOCAL_STORAGE_KEY_USERNAME);
         if (currentUser !== null) {
-            this.centerService.getCenterCodeByUsername(currentUser).subscribe(
-                centerCode => {
-                    this.memberService.getAllMembers(centerCode).subscribe(
-                        response => {
-                            this.allStudents = response;
-                            console.table(response);
-                        },
-                        error => {
-                            console.log(error);
-                        }
-                    )
+            this.centerService.getCenterCodeByUsername(currentUser).pipe(
+                switchMap(centerCode => this.memberService.getAllMembers(centerCode))
+            ).subscribe(
+                response => {
+                    this.allStudents = response;
+                    console.table(response);
                 },
                 error => {
                     console.log(error);
