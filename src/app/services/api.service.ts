79a43d9c00ad1ff96a9be407fd7fa545c2ee7402
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2023-05-24 11:27:09 +0200

    Adds global config for api url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import {environment} from "../../environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ApiService {
-    private url = 'http://127.0.0.1:5000/api/protected';
+    private url = `${environment.apiUrl}protected`;
 
     constructor(private http: HttpClient) { }
 
