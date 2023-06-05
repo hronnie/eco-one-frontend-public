42bdf61514ce9d99ff76f2b58e2e665837016707
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2023-06-05 18:18:28 +0200

    Adds more info to dashboard

--- src/app/interfaces/dashboardInfo.model.ts
+++ src/app/interfaces/dashboardInfo.model.ts
@@ -4,10 +4,16 @@ export interface DashboardInfo {
     totalNoStudentsInMonth: number;
     totalNoStudentsInYear: number;
     studentTrainings: StudentTraining[];
+    studentPrereqTrainings: StudentPrereqTraining[];
 
 }
 
 export interface StudentTraining {
+    name: string,
+    noStudents: number,
+}
+
+export interface StudentPrereqTraining {
     name: string,
     noStudents: number,
 }
